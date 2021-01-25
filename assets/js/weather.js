8df52d473b834fab5a0c974c85dd9d82367c6c2e
WiccaDis <adamrfiles@gmail.com>
2021-01-24 21:38:22 -0600

    having issues with displaying all the api info. only temp will display. will get back to it another day

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,32 +1,25 @@
 var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
+var nameInputEl = document.querySelector("#cityname");
+var weatherContainerEl = document.querySelector("#weather-container");
+
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
   event.preventDefault();
-
   // get value from input element
   var cityname = nameInputEl.value.trim();
-
   if (cityname) {
-    getUserRepos(cityname);
-
+    getCityWeather(cityname);
     // clear old content
-    repoContainerEl.textContent = "";
+    weatherContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
     alert("Please enter city");
   }
 };
 
-var getUserRepos = function(user) {
-  // format the github api url
+var getCityWeather = function(user) {
   var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + user + "&units=imperial&appid=29ca62d72a78d3a80457925d6f338f1c";
-//   var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=austin&appid=29ca62d72a78d3a80457925d6f338f1c";
-
-  // make a get request to url
   fetch(apiUrl)
     .then(function(response) {
       // request was successful
@@ -34,105 +27,86 @@ var getUserRepos = function(user) {
         // console.log(response);
         response.json().then(function(data) {
         //   console.log(data);
-           displayRepos(data.list, user);
-        //   displayWeek(data);
-        //   console.log(data.list[0].main.temp);
-          
+           displayWeather(data, user);
+         //   console.log(data.list[0].main.temp);
         });
       } else {
         alert("Error: " + response.statusText);
-      }
-    })
+      }})
     .catch(function(error) {
       alert("Unable to connect to Weather");
     });
 };
 
-
-
-
-var displayRepos = function(city, searchTerm) {
-    // check if api returned any repos
+var displayWeather = function(city, searchTerm) {
+    // check to see if they type in a city that comes is allowed
     if (city.length === 0) {
-      repoContainerEl.textContent = "No repositories found.";
+      weatherContainerEl.textContent = "No city found";
       return;
     }
-    
-       
-    // loop over repos
+      //  for loop the weather data and then create the elements
     for (var i = 0; i < 5; i++) {
-      // format repo name
-      var condWeather= city[i].weather.main;
-      var condIcon= city[i].weather.icon;
-      var tempWeather= city[i].main.temp;
-      var dateOf= city[i].dt_text;
+      // var used to travers the api
+      var tempWeather= city.list[i].main.temp;
+      var condWeather= city.list[i].weather.main;
+      var condIcon= city.list[i].weather.icon;
+      var dateOf= city.list[i].dt_text;
+      // tests logs
       console.log(condWeather);
       console.log(condIcon);
       console.log(tempWeather);
       console.log(dateOf);
-    //   console.log(tempWeather);
-  
-      // create a container for each repo
+      // create the div that holds the results
       var cityEl = document.createElement("div");
-      cityEl.classList = "list-item flex-row justify-space-between align-center";
+      cityEl.classList = "list-item row flex-row justify-space-between align-center";
       cityEl.textContent = searchTerm;
-
+// displays the date of the weather day
       var dateEl = document.createElement("li")
-      dateEl.classList= "flex-row align-center";
+      dateEl.classList= " col-3 flex-row align-center";
       dateEl.textContent= dateOf;
       cityEl.appendChild(dateEl);
-
+// displays the weather conditions
       var conditionEl = document.createElement("li");
-      conditionEl.classList = "flex-row align-center";
+      conditionEl.classList = " col-3 flex-row align-center";
       conditionEl.textContent = condWeather;
       cityEl.appendChild(conditionEl);
-
+// displays the weather temp
       var tempEl = document.createElement("li");
-      tempEl.classList = "flex-row align-center";
+      tempEl.classList = "col-3 flex-row align-center";
       tempEl.textContent = tempWeather;
       cityEl.appendChild(tempEl);
-
+// displays the icon for the weather
       var iconEl = document.createElement("li");
-      iconEl.classList = "flex-row align-center";
+      iconEl.classList = "col-3 flex-row align-center";
       iconEl.textContent = condIcon;
       cityEl.appendChild(iconEl);
-    //   cityEl.setAttribute("span", searchTerm);
-    //   cityEl.appendChild(conditionEl);
-  
-      // create a span element to hold repository name
+
+// appends it all
       
-  
-      // append to container
-      
-  
-      // create a status element
-    //   var statusEl = document.createElement("span");
-    //   statusEl.classList = "flex-row align-center";
-  
-    //   // check if current repo has issues or not
-    //   if (city[i].open_issues_count > 0) {
-    //     statusEl.innerHTML =
-    //       "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-    //   } else {
-    //     statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    //   }
-  
-      // append to container
-    //   cityEl.appendChild(statusEl);
-  
-      // append container to the dom
-      repoContainerEl.appendChild(cityEl);
+      weatherContainerEl.appendChild(cityEl);
+//  trying to set to local storage
+      // localStorage.setItem(("cityId"+[i]),list[i],)
     }
   };
-  
 
-  
-var displayWeek= function(data){
-    console.log(data);  
+
+  // this is to display the stored cities
+
+  // for (var i = 0; i < length; i++) {
+  //  localStorage.getItem(("cityId"+[i]),list[i],)
+
+
+
+// var displayWeek= function(data){
+//     console.log(data);  
     
-    var heat = data.list+".[0]"
-    for(var i = 0; i < 5; i++ ){
-        console.log(heat.main.temp);
-    }};
+//     var heat = data.list+".[0]"
+//     for(var i = 0; i < 5; i++ ){
+//         console.log(heat.main.temp);
+//     }};
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+
+
+
